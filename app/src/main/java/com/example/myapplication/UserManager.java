8b97dc8f54e69ca1dc0f8f9d91477bc89402498e
Bantonio12 <thcha0727@gmail.com>
2021-10-14 23:46:21 -0400

    finished everything

--- app/src/main/java/com/example/myapplication/UserManager.java
+++ app/src/main/java/com/example/myapplication/UserManager.java
@@ -3,5 +3,25 @@ package com.example.myapplication;
 
 
 public class UserManager {
+    private final UserData data = new UserData();
+    public UserManager() {
 
+    }
+
+    public boolean createUser(String n, String e, String p) {
+        if (!this.data.findUsername(n) && !this.data.findEmail(e)) {
+            this.data.addUserData(new User(n, e, p));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkUser(String n, String p) {
+        if (this.data.findUsername(n)) {
+            return this.data.findCorrespondingPassword(n).equals(p);
+        } else {
+            return false;
+        }
+    }
 }
