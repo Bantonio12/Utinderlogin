6c83cf7d4ad96b7de4a757457ed5736aad03219c
Michael J R <michael.rubenstein@mail.utoronto.ca>
2021-12-01 23:56:01 -0500

    .

--- app/src/main/java/com/example/myapplication/community/CommentActivity.java
+++ app/src/main/java/com/example/myapplication/community/CommentActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication.community;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,5 +41,19 @@ public class CommentActivity extends AppCompatActivity {
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
         listView.setAdapter(arrayAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0) {
+                    startActivity(new Intent(CommentActivity.this, MakingPostActivity.class));
+                } else if(position==1) {
+                    startActivity(new Intent(CommentActivity.this, MakingPostActivity.class));
+                } else {
+                    startActivity(new Intent(CommentActivity.this, MakingPostActivity.class));
+                }
+            }
+        });
         }
 }
