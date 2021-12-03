e8e74212c7fc73551b5be6556b3208feb4d4a231
KexuanZhang <kex.zhang@mail.utoronto.ca>
2021-12-03 00:59:00 -0500

    letsgoooooo!!!!!!!!

--- app/src/main/java/com/example/myapplication/community/MakingPostActivity.java
+++ app/src/main/java/com/example/myapplication/community/MakingPostActivity.java
@@ -1,23 +1,39 @@
 package com.example.myapplication.community;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.myapplication.R;
+import com.example.myapplication.login.user.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MakingPostActivity extends AppCompatActivity {
-
-    String title, text;
 
     EditText titleInput;
     EditText textInput;
 
     Button post_button;
     Button back_button;
+
+
+    private HashMap posts = new HashMap<>();
+    private ArrayList userPosts = new ArrayList<>();
 
 
     @Override
@@ -32,19 +48,71 @@ public class MakingPostActivity extends AppCompatActivity {
         post_button = findViewById(R.id.post_button);
         back_button = findViewById(R.id.back_button);
 
+        //Confirm making post call (update firestore database simultaneously)
         post_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // Get firestore database reference:
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference postsRef = db.collection("community")
+                        .document("Posts");
+                DocumentReference userPostsRef = db.collection("community")
+                        .document("UserPosts");
+
+                String title = titleInput.getText().toString();
+                String text = textInput.getText().toString();
+
+                //write post into firestore Posts document call:
+                postsRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+
+                            DocumentSnapshot document = task.getResult();
+                            posts = (HashMap) document.get("Posts");
+
+                            HashMap newData = new HashMap<>();
+                            posts.put(title, text);
+                            newData.put("Posts", posts);
+                            postsRef.set(newData);
+
+                        }
+                        else {
+                            Log.d("Making Post", "failed to obtain posts", task.getException());
+                        }
+                    }
+                });
+
+                //write post into firestore UserPosts document call:
+                userPostsRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+
+                            DocumentSnapshot document = task.getResult();
+                            userPosts = (ArrayList) document.get("admin");
+
+                            userPosts.add(title);
+                            HashMap newData = new HashMap();
+
+                            newData.put("admin", userPosts);
+
+                            userPostsRef.set(newData);
+                        }
+                        else {
+                            Log.d("Making User Post", "failed to obtain user posts", task.getException());
+                        }
+                    }
+                });
+
                 Intent intent = new Intent(MakingPostActivity.this, CommunityActivity.class);
-                Bundle extras = new Bundle();
-                extras.putString("title", titleInput.getText().toString());
-                extras.putString("text", textInput.getText().toString());
-                intent.putExtras(extras);
                 startActivity(intent);
                 showToast("Successful post");
             }
         });
 
+        //Back to community call:
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/myapplication/community/PostManager.java
+++ app/src/main/java/com/example/myapplication/community/PostManager.java
@@ -6,34 +6,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class PostManager {
-    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final CollectionReference posts = db.collection("community");
 
     public PostManager(){}
-
-    public void makePost(String text, String title){
-        Post post = new Post(text, title);
-        posts.add(post);
-    }
-
-    public ArrayList<Post> loadPosts(){
-        ArrayList<Post> postlist = new ArrayList<>();
-
-        posts.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
-                for (QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots){
-                    Post post = documentSnapshot.toObject(Post.class);
-
-                    postlist.add(post);
-                }
-            }
-        });
-
-        return postlist;
-    }
-
     public void makeComment(String text/*, User postMaker*/, Post post){//not include user for now!!
         Post comment = new Post(text/*, postMaker*/);//not include user for now!!
         post.addComment(comment);
